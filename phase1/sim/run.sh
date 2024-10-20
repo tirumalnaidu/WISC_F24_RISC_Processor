0592f354e8247f5f49b5eb1f258f7af17c0e2f4e
Tirumal Naidu <tirumal.bodepu@gmail.com>
2024-10-20 15:31:14 -0500

    changed dir paths in iverilog command

--- phase1/sim/run.sh
+++ phase1/sim/run.sh
@@ -17,4 +17,4 @@ perl $BASE/WISC-assembler/assembler.pl $LIST > loadfile_instr.img
 touch loadfile_data.img # empty data memory image
 
 # This is for Icarus Verilog
-iverilog -g2001 $BASE/dv/phase1_cpu_tb.v $BASE/ip/*.v $BASE/design/*.v && vvp a.out
+iverilog -g2001 $BASE/dv/phase1_cpu_tb.v $BASE/design/common/flags.vh $BASE/design/common/*.v $BASE/design/*.v && vvp a.out
