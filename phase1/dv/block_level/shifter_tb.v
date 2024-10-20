37edbc638c461fe8c3f9cd1d4199e8dbe6b029d2
bvenkatarama <bvenkatarama@wisc.edu>
2024-10-19 19:34:00 -0500

    Adding block level tests - shifter

---
+++ phase1/dv/block_level/shifter_tb.v
@@ -0,0 +1,15 @@
+`timescale 1ns/1ns;
+module shifter_tb();
+    reg [15:0]  shift_in;
+    reg [3:0]   shift_val;
+    reg [1:0]   mode;
+    wire [15:0] shift_out;
+    wire [2:0]  flag;
+
+    initial begin
+    end
+
+    initial begin
+    end
+
+endmodule
