4e03eee5bd51405b43dfb2d5ef98ff2a838315f2
Yash Deshpande <23yashd@gmail.com>
2024-11-17 08:24:29 -0600

    load-to-use stalls completed

--- phase2/design/hazard_unit.v
+++ phase2/design/hazard_unit.v
@@ -1,39 +1,43 @@
 module hazard_detection_unit(
 
-    // ---------- Maybe Not Needed ------------
-    // input [3:0] ifid_reg_rs,
-    // input [3:0] ifid_reg_rt, 
-
-    // input [3:0] idex_write_reg,
-    // input [3:0] exmem_write_reg,
-    // input [3:0] memwb_write_reg,
-
-    // input idex_reg_write,
-    // input exmem_reg_write,
-    // input memwb_reg_write,
-
-    // ---------- Maybe Not Needed ------------
-
-    input idex_mem_read,
+    input id_ex_mem_read,
+    input if_id_rs,
+    input if_id_rt,
+    input id_ex_rt,
     
-    output pc_write,            // write_enable signal to pc_update register
-    output ifid_write           // write_enable signal to the IF/ID. pipeline register
+    output pc_wen,            // write_enable signal to pc_update register
+    output if_id_wen,         // write_enable signal to the IF/ID. pipeline register
 
-    output stall                // global stall if needed  
+    input branch,
+    input branchr,
+
+    output global_stall                // global stall if needed  
 
 );
 
-// forwarding
-// assign stall = (idex_reg_write==1 && ifid_reg_rs==idex_write_reg)? 1'b1 : 
-//                (idex_reg_write==1 && ifid_reg_rt==idex_write_reg)? 1'b1 : 
-//                (exmem_reg_write==1 && ifid_reg_rs==exmem_write_reg)? 1'b1 : 
-//                (exmem_reg_write==1 && ifid_reg_rt==exmem_write_reg)? 1'b1 : 
-//                (memwb_reg_write==1 && ifid_reg_rs==memwb_write_reg)? 1'b1 : 
-//                (memwb_reg_write==1 && ifid_reg_rt==memwb_write_reg)? 1'b1 : 1'b0;
+wire stall
+
+// load-to-use stalls : COD pg314
+assign stall = (id_ex_mem_read==1)? 
+               (if_id_rs==id_ex_rt) | (if_id_reg_rt==id_ex_rt)? 1'b1 : 1'b0;
+
+// branching stalls according to changes in flag register value
 
 
-// load-to-use stalls
-assign stall = (idex_mem_read==1 && ifid_reg_rs==idex_write_reg)? 1'b1 : 
-               (idex_mem_read==1 && ifid_reg_rt==idex_write_reg)? 1'b1 : 1'b0
+
+// stall the pipeline
+assign pc_wen = ~stall;
+assign if_id_wen = ~stall;
 
 endmodule
+
+/*
+Load-to-Use Stall  
+e.g.  sub after load
+1. load is in ex stage so gets its register values from id_ex pipeline register
+2. sub is in decode so gets its values from the if_id pipeline register
+
+
+*/
+
+
